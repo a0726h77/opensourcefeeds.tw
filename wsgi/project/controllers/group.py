fcb33f99556e93b70b9fe242c13f220c9c167a66
yan <a0726h77@gmail.com>
2014-08-26 10:04:20 +0800

    add group index page

--- wsgi/project/controllers/group.py
+++ wsgi/project/controllers/group.py
@@ -9,6 +9,13 @@ from project.models.groups import Groups
 from project.models.group_types import GroupTypes
 
 
+@app.endpoint('group.page')
+def page(group_id):
+    group = db.session.query(Groups, GroupTypes).filter(Groups.type == GroupTypes.id, Groups.id == group_id).one()
+
+    return render_template('group/page.html', group=group)
+
+
 @app.endpoint('group.all_html')
 def all_html():
     groups = db.session.query(Groups, GroupTypes).filter(Groups.type == GroupTypes.id).order_by(Groups.type, Groups.name).all()
--- wsgi/project/url.py
+++ wsgi/project/url.py
@@ -5,6 +5,7 @@ from werkzeug.routing import Rule
 
 urlpatterns = {
     Rule('/', endpoint='index'),
+    Rule('/group/<group_id>', endpoint='group.page'),
     Rule('/group/html', endpoint='group.all_html'),
     Rule('/event/rss', endpoint='event.rss'),
 }
