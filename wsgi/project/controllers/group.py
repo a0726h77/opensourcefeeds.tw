bd25577f0a039af563184cf2ccdc89514fbf2a1f
yan <a0726h77@gmail.com>
2014-08-26 13:02:12 +0800

    list group events

--- wsgi/project/controllers/group.py
+++ wsgi/project/controllers/group.py
@@ -3,11 +3,13 @@
 from project import app
 from flask import Flask, request, session, url_for, redirect, \
      render_template, abort, g, flash, _app_ctx_stack
+import datetime
 
 from project.models.models import db
 from project.models.groups import Groups
 from project.models.group_types import GroupTypes
 from project.models.group_websites import GroupWebsites
+from project.models.events import Events
 
 
 @app.endpoint('group.page')
@@ -24,7 +26,11 @@ def page(group_id):
         else:
             group_websites_no_icon.append(group_website)
 
-    return render_template('group/page.html', group=group, group_websites_no_icon=group_websites_no_icon, group_websites_has_icon=group_websites_has_icon)
+    recent_events = Events.query.filter(Events.group_id == group_id, Events.start_datetime > datetime.datetime.now()).order_by(Events.start_datetime).all()
+
+    past_events = Events.query.filter(Events.group_id == group_id, Events.start_datetime < datetime.datetime.now()).order_by(Events.start_datetime.desc()).limit(8)
+
+    return render_template('group/page.html', group=group, group_websites_no_icon=group_websites_no_icon, group_websites_has_icon=group_websites_has_icon, recent_events=recent_events, past_events=past_events)
 
 
 @app.endpoint('group.all_html')
