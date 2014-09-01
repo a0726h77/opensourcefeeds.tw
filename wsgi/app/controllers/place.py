045fd46172d245181e4c7158199ed39a457559ed
yan <a0726h77@gmail.com>
2014-09-01 18:21:04 +0800

    search by geometry

--- wsgi/app/controllers/place.py
+++ wsgi/app/controllers/place.py
@@ -20,10 +20,22 @@ def cafe_index():
         places = None
 
         if 'station' in request.form and request.form['station']:  # 臨近捷運站
-            places = Places.query.filter(Places.mrt == request.form['station'], Places.poi_type == poi_type.id).all()
+            # places = Places.query.filter(Places.mrt == request.form['station'], Places.poi_type == poi_type.id).all()
+
+            scale = 0.6
+            Places.coords = classmethod(lambda s: (s.lat, s.lng))
+
+            mrt = Places.query.filter(Places.id == request.form['station']).one()
+
+            places = Places.query.filter(db.or_(calc_distance(Places.coords(), (mrt.lat, mrt.lng)) < scale, Places.mrt == request.form['station']), Places.poi_type == poi_type.id).all()
         elif 'name' in request.form:  # 關鍵字查詢
             places = Places.query.filter(db.or_(Places.name.like("%%%s%%" % request.form['name']), Places.address.like("%%%s%%" % request.form['name']))).all()
 
         return render_template('place/cafe_list.html', places=places)
 
     return ''
+
+
+def calc_distance(latlong1, latlong2):
+    return db.func.sqrt(db.func.pow(69.1 * (latlong1[0] - latlong2[0]), 2)
+                      + db.func.pow(53.0 * (latlong1[1] - latlong2[1]), 2))
