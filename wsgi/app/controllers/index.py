17b4242bf3c1faa8bb07c932eaaff14b2ef5e64d
yan <a0726h77@gmail.com>
2014-09-01 14:22:15 +0800

    star user interest group event

--- wsgi/app/controllers/index.py
+++ wsgi/app/controllers/index.py
@@ -9,13 +9,21 @@ from app.models.models import db
 from app.models.groups import Groups
 from app.models.group_types import GroupTypes
 from app.models.events import Events
+from app.models.user_star_group import UserStarGroup
 
 
 @app.endpoint('index')
 def index():
     recent_events = db.session.query(Groups, Events).filter(Groups.id == Events.group_id, Events.start_datetime > datetime.datetime.now()).order_by(Events.start_datetime).all()
 
-    return render_template('index.html', recent_events=recent_events)
+    ## user start groups ##
+    star_groups = None
+    if 'user_id' in session:
+        star_groups = UserStarGroup.query.filter(UserStarGroup.user_id == session['user_id']).all()
+        star_groups = [star_group.group_id for star_group in star_groups]
+    ## user start groups ##
+
+    return render_template('index.html', recent_events=recent_events, star_groups=star_groups)
     # return redirect(url_for('group.all_html'))
 
 
